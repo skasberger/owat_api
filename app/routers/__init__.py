d6431240c291fa288b7423761582c44f7be36af5
skasberger <mail@stefankasberger.at>
2020-11-07 17:53:46 +0100

    Remove async from routers

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -22,5 +22,5 @@ router.include_router(votes.router, prefix="/elections", tags=["elections"])
 
 
 @router.get("/")
-async def get_root():
+def get_root():
     return {"version": "1.0", "status": "OK", "name": "offenewahlen-at"}
--- app/routers/elections.py
+++ app/routers/elections.py
@@ -12,17 +12,17 @@ router = APIRouter()
 
 
 @router.get("/", tags=["elections"])
-async def get_elections(db: Session = Depends(get_db)):
+def get_elections(db: Session = Depends(get_db)):
     return crud.get_elections(db=db)
 
 
 @router.get("/{election_id}", tags=["elections"])
-async def get_election(election_id: int, db: Session = Depends(get_db)):
+def get_election(election_id: int, db: Session = Depends(get_db)):
     return crud.get_election(db=db, election_id=election_id)
 
 
 @router.post("/", response_model=Election, tags=["elections"])
-async def create_election(election: Election, db: Session = Depends(get_db)):
+def create_election(election: Election, db: Session = Depends(get_db)):
     db_election = crud.get_election_by_acronym(db, acronym=election.acronym)
     if db_election:
         raise HTTPException(status_code=400, detail="Election already registered")
