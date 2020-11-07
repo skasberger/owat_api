66119aefe4a1ad04b5e7fcdc95afdddcd265bd49
skasberger <mail@stefankasberger.at>
2020-11-07 13:07:27 +0100

    Implement config loading.

--- app/__init__.py
+++ app/__init__.py
@@ -9,26 +9,39 @@ Licensed under the MIT License.
 from fastapi import FastAPI
 from . import models
 from .database import init_db
+from config import config
+import os
 
 __author__ = "Stefan Kasberger"
 __email__ = "mail@stefankasberger.at"
 __copyright__ = "Copyright (c) 2020 Stefan Kasberger"
 __license__ = "MIT License"
 __version__ = "0.0.1"
-__url__ = ""
-__download_url__ = ""
-__description__ = ""
+__url__ = "https://github.com/skasberger/owat_api"
+__download_url__ = "https://github.com/skasberger/owat_api"
+__description__ = "RESTful API for Austrian open elections data"
 
 
 def create_app(config_name="default"):
     """Create application and load settings."""
     print("* Start Offene Wahlen AT API...")
 
-    app = FastAPI(title="owat_api")
+    FASTAPI_ENV = os.environ.get("FASTAPI_ENV")
+    SQLALCHEMY_DATABASE_URI = config[FASTAPI_ENV].SQLALCHEMY_DATABASE_URI
+    DEBUG = config[FASTAPI_ENV].DEBUG
+    API_PREFIX = config[FASTAPI_ENV].API_PREFIX
+    SECRET_KEY = config[FASTAPI_ENV].SECRET_KEY
+    ADMIN_EMAIL = config[FASTAPI_ENV].ADMIN_EMAIL
+    APP_EMAIL = config[FASTAPI_ENV].APP_EMAIL
+    TRAVIS = config[FASTAPI_ENV].TRAVIS
+    MIN_CONNECTIONS_COUNT = config[FASTAPI_ENV].MIN_CONNECTIONS_COUNT
+    MAX_CONNECTIONS_COUNT = config[FASTAPI_ENV].MAX_CONNECTIONS_COUNT
+
+    app = FastAPI(title="owat_api", debug=DEBUG)
 
     from app.routers import router as api_router
 
-    app.include_router(api_router, prefix="/api")
+    app.include_router(api_router, prefix=API_PREFIX)
 
     # init_db()
     from app.database import Base, engine
---
+++ config.py
@@ -0,0 +1,152 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    """
+    Setting the default environment settings.
+    """
+
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    DEBUG = os.environ.get("DEBUG") or False
+    API_PREFIX = "/api"
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "my-secret-key"
+    ADMIN_EMAIL = os.environ.get("ADMIN_EMAIL") or None
+    APP_EMAIL = os.environ.get("APP_EMAIL") or None
+    TRAVIS = False
+    MIN_CONNECTIONS_COUNT = os.environ.get("MIN_CONNECTIONS_COUNT") or None
+    MAX_CONNECTIONS_COUNT = os.environ.get("MAX_CONNECTIONS_COUNT") or None
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    """
+    Setting the development environment settings.
+    Database is sqlite file or a postgresql database string passed by an environment variable.
+    """
+
+    SQLALCHEMY_DATABASE_URI = (
+        os.environ.get("DEV_DATABASE_URI") or "postgresql://localhost/owat_dev"
+    )
+
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+
+class TestingConfig(Config):
+    SQLALCHEMY_DATABASE_URI = (
+        os.environ.get("TEST_DATABASE_URI") or "postgresql://localhost/owat_test"
+    )
+
+    ADMIN_EMAIL = "testing@offenewahlen.at"
+    APP_EMAIL = "testing@offenewahlen.at"
+
+
+class TravisConfig(TestingConfig):
+    """
+    Setting the test environment settings.
+    """
+
+    TRAVIS = True
+    # SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI = (
+        "postgresql+psycopg2://postgres@localhost:5432/travis_ci_test"
+    )
+
+
+class ProductionConfig(Config):
+    """
+    Setting the production environment settings.
+    """
+
+    SQLALCHEMY_DATABASE_URI = (
+        os.environ.get("PROD_DATABASE_URI") or "postgresql://localhost/owat"
+    )
+
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+        import logging
+        from logging.handlers import RotatingFileHandler
+
+        if not os.path.exists("logs"):
+            os.mkdir("logs")
+
+        file_handler = RotatingFileHandler(
+            "logs/owatapi.log", maxBytes=10240, backupCount=10
+        )
+
+        file_handler.setFormatter(
+            logging.Formatter(
+                "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+            )
+        )
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info("Offene Wahlen AT API")
+
+
+class DockerConfig(ProductionConfig):
+    SQLALCHEMY_DATABASE_URI = (
+        os.environ.get("DOCKER_DATABASE_URI") or "postgresql://localhost/owat"
+    )
+
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        # log to stderr
+        import logging
+        from logging import StreamHandler
+
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+
+class DockerComposeConfig(DockerConfig):
+    SQLALCHEMY_DATABASE_URI = (
+        os.environ.get("DOCKER_COMPOSE_DATABASE_URI")
+        or "postgresql://postgres:postgres@localhost/owat"
+    )
+
+    @classmethod
+    def init_app(cls, app):
+        DockerConfig.init_app(app)
+
+
+class UnixConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        # log to syslog
+        import logging
+        from logging.handlers import SysLogHandler
+
+        syslog_handler = SysLogHandler()
+        syslog_handler.setLevel(logging.INFO)
+        app.logger.addHandler(syslog_handler)
+
+
+config = {
+    "development": DevelopmentConfig,
+    "testing": TestingConfig,
+    "production": ProductionConfig,
+    "travis": TravisConfig,
+    "docker": DockerConfig,
+    "docker_compose": DockerComposeConfig,
+    "unix": UnixConfig,
+    "default": DevelopmentConfig,
+}
