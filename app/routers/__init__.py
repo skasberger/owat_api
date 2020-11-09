5f1e97904ee2f8cdc3c066009c0f2d597de09867
skasberger <mail@stefankasberger.at>
2020-11-09 11:35:37 +0100

    Fix issue: loading of routes not in repo

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -3,22 +3,24 @@
 """"""
 from fastapi import APIRouter
 
-import app.routers.districts
+# import app.routers.districts
 import app.routers.elections
-import app.routers.municipalities
+
+# import app.routers.municipalities
 import app.routers.parties
-import app.routers.states
-import app.routers.votes
+
+# import app.routers.states
+# import app.routers.votes
 
 router = APIRouter()
-router.include_router(districts.router, prefix="/districts", tags=["districts"])
+# router.include_router(districts.router, prefix="/districts", tags=["districts"])
 router.include_router(elections.router, prefix="/elections", tags=["elections"])
-router.include_router(
-    municipalities.router, prefix="/municipalities", tags=["municipalities"]
-)
+# router.include_router(
+#     municipalities.router, prefix="/municipalities", tags=["municipalities"]
+# )
 router.include_router(parties.router, prefix="/parties", tags=["parties"])
-router.include_router(states.router, prefix="/states", tags=["states"])
-router.include_router(votes.router, prefix="/elections", tags=["elections"])
+# router.include_router(states.router, prefix="/states", tags=["states"])
+# router.include_router(votes.router, prefix="/votes", tags=["votes"])
 
 
 @router.get("/")
